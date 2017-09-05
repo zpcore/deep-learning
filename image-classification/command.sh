259c0ecd2d7763cc4e9484a47bcf776089a5255b
zpcore <peizhang@iastate.edu>
2017-09-05 00:15:51 -0500

    project 2

---
+++ image-classification/command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/VYNyoPVSUfXsJn7maMbJvR' --NotebookApp.token='' --NotebookApp.allow_origin='*'
